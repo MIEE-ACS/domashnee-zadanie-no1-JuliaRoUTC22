754de38a4f59b3499b250f6c6c3c8839bd24ba44
Julia Rozhkova <71033750+JuliaRoUTC22@users.noreply.github.com>
2020-09-17 21:19:49 +0300

    Домашнее задание.

---
+++ .github/Console_homework_1/Console_homework_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Console_homework_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            string A = Console.ReadLine();
+            Console.WriteLine("\n Результат:");
+            double d = double.Parse(A); //квадрат введённого числа
+            d = d * d;
+            Console.WriteLine($"{d} - A во 2-ой степени");
+            d = d * d;
+            Console.WriteLine($"{d} - A в 4-ой степени");
+            d = d * d;
+            Console.WriteLine($"{d} - A в 8-ой степени");
+            Console.ReadLine();
+        }
+    }
+}
